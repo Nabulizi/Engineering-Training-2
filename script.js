a173ad8d6210708d99422758df7e4c5fffa4edb7
Nueraili Abulizi <55996722+Nabulizi@users.noreply.github.com>
2023-01-23 12:16:09 -0500

    TT-18 JavaScript: Event Listeners - Add Toggle Button Inside of Modal

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 console.log("Engineering Training");
 const modalButton = document.getElementById("modalButton");
+const modalContainer = document.getElementById("modalContainer");
+modalButton.addEventListener("click", () => {
+  console.log("clicked button!!");
+});
+modalButton.addEventListener("click", () => {
+  const list = modalContainer.classList;
+  list.toggle("hidden");
+});
 console.log("modalButton : ", modalButton);
