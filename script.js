039f33f6aa1811a223efd66f47f2114e5561d2d7
Nueraili Abulizi <55996722+Nabulizi@users.noreply.github.com>
2023-01-24 10:42:31 -0500

    TT-19 JavaScript: getElementsByClassName - close modal Part 1

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 console.log("Engineering Training");
 const modalButton = document.getElementById("modalButton");
 const modalContainer = document.getElementById("modalContainer");
+const closeModalButton = document.getElementsByClassName("close-modal-button");
 modalButton.addEventListener("click", () => {
   console.log("clicked button!!");
 });
 modalButton.addEventListener("click", () => {
-  const list = modalContainer.classList;
-  list.toggle("hidden");
+  modalContainer.classList.toggle("hidden");
+});
+
+closeModalButton[0].addEventListener("click", () => {
+    modalContainer.classList.toggle("hidden");
 });
 console.log("modalButton : ", modalButton);
+console.log("closeModalButton : ", closeModalButton);
