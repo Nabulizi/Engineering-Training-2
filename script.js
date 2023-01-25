19038dd0bd24c0edd4a3cb6e87747cac899a60f7
Nueraili Abulizi <55996722+Nabulizi@users.noreply.github.com>
2023-01-25 14:08:43 -0500

    TT-20 JavaScript: Arrays

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ const modalButton = document.getElementById("modalButton");
 const modalContainer = document.getElementById("modalContainer");
 const closeModalButton = document.getElementsByClassName("close-modal-button");
 
+const jiraLinks = [
+  "https://totalwine.atlassian.net/browse/TT-2",
+  "https://totalwine.atlassian.net/browse/TT-16",
+  "https://totalwine.atlassian.net/browse/TT-17",
+  "https://totalwine.atlassian.net/browse/TT-18",
+  "https://totalwine.atlassian.net/browse/TT-19",
+];
+const jiraTitles = [
+  "Create a public repository under your GitHub account",
+  "Create a new script file, and import it into index.html and add a console log",
+  "JavaScript: Variables",
+  "JavaScript: Event Listeners - Add Toggle Button Inside of Modal",
+  "JavaScript: Functions - Write a function to toggle hidden class on modal",
+];
+
+console.log("jiraTitles", jiraTitles);
+console.log("jiraLinks", jiraLinks);
+
 modalButton.addEventListener("click", () => {
   console.log("clicked button!!");
 });
@@ -12,7 +30,7 @@ modalButton.addEventListener("click", () => {
 });
 
 closeModalButton[0].addEventListener("click", () => {
-    modalContainer.classList.toggle("hidden");
+  modalContainer.classList.toggle("hidden");
 });
 
 console.log("modalButton : ", modalButton);
