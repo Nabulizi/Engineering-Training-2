9c0a58e47c7aea29b9574934c193a896c008cfd3
Nueraili Abulizi <55996722+Nabulizi@users.noreply.github.com>
2023-01-25 10:24:45 -0500

    TT-68 https://totalwine.atlassian.net/browse/TT-68

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ console.log("Engineering Training");
 const modalButton = document.getElementById("modalButton");
 const modalContainer = document.getElementById("modalContainer");
 const closeModalButton = document.getElementsByClassName("close-modal-button");
+
 modalButton.addEventListener("click", () => {
   console.log("clicked button!!");
 });
+
 modalButton.addEventListener("click", () => {
   modalContainer.classList.toggle("hidden");
 });
 
 closeModalButton[0].addEventListener("click", () => {
     modalContainer.classList.toggle("hidden");
+    console.log("closeModalButton is closed");
 });
+
 console.log("modalButton : ", modalButton);
 console.log("closeModalButton : ", closeModalButton);
