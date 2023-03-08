e78b2a68f52b7598afaebff326c3edf12569e830
Nueraili Abulizi <55996722+Nabulizi@users.noreply.github.com>
2023-03-08 13:34:21 -0500

    TT-25 Functions - Render List After Timeout

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log("Engineering Training");
 const modalButton = document.getElementById("modalButton");
 const modalContainer = document.getElementById("modalContainer");
 const closeModalButton = document.getElementsByClassName("close-modal-button");
+let dataLoaded = true;
 
 const jiraLinks = [
   "https://totalwine.atlassian.net/browse/TT-2",
@@ -27,23 +28,31 @@ for (let i = 0; i < jiraLinks.length; i++) {
   });
 }
 
-jirasArray.forEach((element) => {
-  console.log("jira: ", element);
-  let listItem = document.createElement("li");
-  let listElement = document.querySelector(".grid-container");
+function renderData() {
+  jirasArray.forEach((element) => {
+    console.log("jira: ", element);
+    let listItem = document.createElement("li");
+    let listElement = document.querySelector(".grid-container");
 
-  const myLiterals = `<li class="item">
-  <a href=${element.link}>
-  <i class="bi bi-check-circle-fill"></i>${element.title}</a>
-  </li>`;
+    const myLiterals = `<li class="item">
+    <a href=${element.link}>
+    <i class="bi bi-check-circle-fill"></i>${element.title}</a>
+    </li>`;
 
-  listItem.innerHTML = myLiterals; // Assigning myLiterals to innerHTML of listItem
-  listElement.append(listItem); // Appending listItem to listElement
-});
+    listItem.innerHTML = myLiterals; // Assigning myLiterals to innerHTML of listItem
+    listElement.append(listItem); // Appending listItem to listElement
+  });
+}
 
 function loadData() {
   setTimeout(() => {
     console.log("Data Loaded");
+    modalContainer.classList.toggle("hidden");
+    if(dataLoaded==true){
+      renderData();
+      dataLoaded=false;
+    }
+    
   }, 1000);
 }
 
